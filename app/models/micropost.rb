412d7a36276d23bfafc34b525675ae27848e4c4b
Mehmet Öğütcan <mogutcan@bil.omu.edu.tr>
2012-03-18 23:01:59 +0200

    Karakter Uzunlugu

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,6 @@
 class Micropost < ActiveRecord::Base
 
-  validates :content , :length => { :maximum => 100 }
+  validates :content , :length => { :maximum => 100000000000000 }
 
   validates :content , :length => { :minimum => 5 }
 
